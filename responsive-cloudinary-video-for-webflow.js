dcd7b04c152cc8624ea23d01927e410042a33900
Georgy Rakita <rakitaegor@gmail.com>
2024-05-28 00:38:12 -0300

    Updating version number in code

--- responsive-cloudinary-video-for-webflow.js
+++ responsive-cloudinary-video-for-webflow.js
@@ -1,4 +1,4 @@
-//1.0.1
+//1.0.3
 document.addEventListener("DOMContentLoaded", function () {
     const defaultBreakpoints = "1920, 1440, 1280, 992, 768, 480";
     const cloudName = (document.querySelector('[r-video_cloud-name]') || {}).getAttribute('r-video_cloud-name') || null;
